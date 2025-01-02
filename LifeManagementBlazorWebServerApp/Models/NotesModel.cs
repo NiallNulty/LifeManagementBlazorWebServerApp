ad8035823d88d07608b7abe7decdb7dbaac823ec
Niall Nulty <niallnulty@gmail.com>
2025-01-02 22:34:13 +0000

    added support for creating, updating & deleting notes

---
+++ LifeManagementBlazorWebServerApp/Models/NotesModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LifeManagementBlazorWebServerApp.Models
+{
+    public class NotesModel
+    {
+        public string Title { get; set; }
+        public string Note { get; set; }
+    }
+}
